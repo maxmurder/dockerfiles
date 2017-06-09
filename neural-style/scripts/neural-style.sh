a8caa4a96360292a479e2b9d983b84f967479f72
U-RAINMAKER\ccampbell <ccampbell@D0079.rainmaker.com>
2017-06-09 09:20:36 -0700

    fix typo

--- neural-style/scripts/neural-style.sh
+++ neural-style/scripts/neural-style.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-th /neural-style/neural_style.lua -prota_file /neural-style/models/VGG_ILSVRC_19_layers_deploy.prototxt -model_file /neural-style/models/VGG_ILSVRC_19_layers.caffemodel "$@"
+th /neural-style/neural_style.lua -proto_file /neural-style/models/VGG_ILSVRC_19_layers_deploy.prototxt -model_file /neural-style/models/VGG_ILSVRC_19_layers.caffemodel "$@"
